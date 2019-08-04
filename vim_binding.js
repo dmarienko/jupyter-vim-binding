0ec8d2d14b228660206624cc7ab1c294869534cb
dima_neptun <dmitry.ema@gmail.com>
2019-08-04 18:32:29 +0200

    My keys

--- vim_binding.js
+++ vim_binding.js
@@ -88,7 +88,72 @@ define([
     for(var i=0; i<this.on_ready_callbacks.length; i++) {
       this.on_ready_callbacks[i](this);
     }
+	
+	// comment/uncomment
+    CodeMirror.Vim.defineAction("toggle_comment_a", function(cm) {
+        cm.toggleComment();
+    });
+    CodeMirror.Vim.mapCommand("\\", "action", "toggle_comment_a", {});
+
+	 CodeMirror.Vim.map("jj", "<Esc>", "insert");
+	  // Swap j/k and gj/gk (Note that <Plug> mappings)
+	  CodeMirror.Vim.map("j", "<Plug>(vim-binding-gj)", "normal");
+	  CodeMirror.Vim.map("k", "<Plug>(vim-binding-gk)", "normal");
+	  CodeMirror.Vim.map("gj", "<Plug>(vim-binding-j)", "normal");
+	  CodeMirror.Vim.map("gk", "<Plug>(vim-binding-k)", "normal");
   };
+
+
+// ----------------------------------------------------------
+	
+CodeMirror.Vim.defineAction('[i]<C-h>', function(cm) {
+  var head = cm.getCursor();
+  CodeMirror.Vim.handleKey(cm, '<Esc>');
+  if (head.ch <= 1) {
+    CodeMirror.Vim.handleKey(cm, 'i');
+  } else {
+    CodeMirror.Vim.handleKey(cm, 'h');
+    CodeMirror.Vim.handleKey(cm, 'a');
+  }
+});
+
+CodeMirror.Vim.defineAction('[i]<C-l>', function(cm) {
+  var head = cm.getCursor();
+  CodeMirror.Vim.handleKey(cm, '<Esc>');
+  if (head.ch === 0) {
+    CodeMirror.Vim.handleKey(cm, 'a');
+  } else {
+    CodeMirror.Vim.handleKey(cm, 'l');
+    CodeMirror.Vim.handleKey(cm, 'a');
+  }
+});
+
+CodeMirror.Vim.mapCommand("<C-h>", "action", "[i]<C-h>", {}, { "context": "insert" });
+CodeMirror.Vim.mapCommand("<C-l>", "action", "[i]<C-l>", {}, { "context": "insert" });
+CodeMirror.Vim.map("<C-j>", "<Esc>ja", "insert");
+CodeMirror.Vim.map("<C-k>", "<Esc>ka", "insert");
+
+// Use Ctrl-h/l/j/k to move around in Normal mode
+// otherwise it would trigger browser shortcuts
+CodeMirror.Vim.map("<C-h>", "h", "normal");
+CodeMirror.Vim.map("<C-l>", "l", "normal");
+//CodeMirror.Vim.map("<C-j>", "j", "normal");
+//CodeMirror.Vim.map("<C-k>", "k", "normal");
+
+
+// ----------------------------------------------------------
+require([
+  'nbextensions/vim_binding/vim_binding',
+  'base/js/namespace',
+], function(vim_binding, ns) {
+  // Add post callback
+  vim_binding.on_ready_callbacks.push(function(){
+    var km = ns.keyboard_manager;
+    // Indicate the key combination to run the commands
+    km.edit_shortcuts.add_shortcut('alt-;', 'jupyter-notebook:run-cell', true);
+    km.edit_shortcuts.events.trigger('rebuild.QuickHelp');
+  });
+});
 
   exports.detach = function detach() {
     for(var i=0; i<modules.length; i++) {
@@ -105,3 +170,5 @@ define([
 
   return exports;
 });
+
+
