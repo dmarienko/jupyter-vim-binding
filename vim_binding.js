fda21012109aa6a3e35be95e931c12012012e53c
dima_neptun <dmitry.ema@gmail.com>
2019-08-12 22:36:33 +0200

    Alt-I

--- vim_binding.js
+++ vim_binding.js
@@ -70,6 +70,7 @@ define([
     cm_config.extraKeys = $.extend(cm_config.extraKeys || {}, {
       'Esc': CodeMirror.prototype.leaveInsertMode,
       'Shift-Esc': CodeMirror.prototype.leaveNormalMode,
+      'Alt-I': CodeMirror.prototype.leaveNormalMode,
       'Ctrl-C': false,  // To enable clipboard copy
     });
 
