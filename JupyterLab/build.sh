189646a89ee6e45d31d3f77b79ebb4fe536ea0ee
quant1 <dmitry.ema@gmail.com>
2020-01-29 05:30:06 -0600

    JLab vim extension

---
+++ JupyterLab/build.sh
@@ -0,0 +1,4 @@
+tar czf jupyterlab_vim-0.13.x.tgz package
+sudo cp jupyterlab_vim-0.13.x.tgz ~/.jupyter/lab/application/extensions
+#sudo /usr/local/envs/ira_p36/bin/jupyter lab build
+jupyter lab build
