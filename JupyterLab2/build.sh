8cd89fe61126412b274ea7d7d47b9d89dec26379
quant1 <dmitry.ema@gmail.com>
2020-05-11 11:27:28 +0200

    jl2

---
+++ JupyterLab2/build.sh
@@ -0,0 +1,4 @@
+tar czf jupyterlab_vim-0.13.0.tgz package
+sudo cp jupyterlab_vim-0.13.0.tgz ~/.jupyter/lab/application/extensions
+#sudo /usr/local/envs/ira_p36/bin/jupyter lab build
+jupyter lab build
